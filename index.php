d5010ae71596386cf1bc5181df546ae61491b885
unknown <niconzeweb@wanadoo.fr>
2019-03-18 14:34:52 +0100

    carouselAvisFini

--- index.php
+++ index.php
@@ -35,8 +35,10 @@
             var y = document.getElementById('carouselOne');
             if(x.style.display === "none"){
                 x.style.display="block";
+                y.style.display="none";
             }else{
                 x.style.display="none";
+                y.style.display="block";
             }
         }
 
